d8d0392d3503bda823c79af83f3a9bec2b712bb7
VanessaBuechner <vanessa.buechner@code.berlin>
2024-04-22 12:00:33 +0200

    error handling

--- app.js
+++ app.js
@@ -192,8 +192,8 @@ app.post('/search', function (request, response){
                 const lyrics_musix_match = `${api_link_musix_match}track.lyrics.get?track_id=${track_id}&apikey=${token_musix_match}`
                 return fetch (lyrics_musix_match) 
             }else{
-                console.log(json_id.message.header)
-                throw 'There is no artist or track in the musixmatch database'
+                throw json_id.message.header
+                // throw 'There is no artist or track in the musixmatch database'
             }  
         })
         .then(response_lyrics => response_lyrics.json())
@@ -204,18 +204,19 @@ app.post('/search', function (request, response){
                 // lyrics = lyrics.split('*******')[0].split('\n')
                 response.render('index', {lyrics: lyrics, scroll: '.display-start'})
             }else{
-                console.log(json_lyrics.message.header)
-                throw 'The artist or track does not have lyrics'
+                throw json_lyrics.message.header
+                // throw 'The artist or track does not have lyrics'
             } 
         })
         .catch((err) => {
             console.log(err)
-            response.redirect('/error')
+            response.redirect('/error', {error_log: err})
         })
  
 })
 
 app.get('/error', (request, response) => {
+    console.log(error_log)
     response.send(`The artist or track does not have lyrics`)
 })
 
